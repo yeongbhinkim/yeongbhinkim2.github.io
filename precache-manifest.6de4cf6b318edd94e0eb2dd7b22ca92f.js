c19e4874a11d5f1b239b7db9d0fc4d57a63c37a4
김영빈 <93468838+yeongbhinkim@users.noreply.github.com>
2024-06-28 13:55:00 +0900

    Add files via upload

---
+++ precache-manifest.6de4cf6b318edd94e0eb2dd7b22ca92f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6bdb37dfb703d073c1c672887495de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28048336c64a0f011b5a",
+    "url": "/static/css/main.b878c319.chunk.css"
+  },
+  {
+    "revision": "c72feb6dc286339a449b",
+    "url": "/static/js/2.38fec76d.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "28048336c64a0f011b5a",
+    "url": "/static/js/main.626f668c.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
